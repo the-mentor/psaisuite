aed3d2fbdee72b175f1643cead061545ef5a3f89
dfinke <finked@hotmail.com>
2025-03-09 15:53:58 -0400

    Add PowerShell script to demonstrate parallel model prompting with Groq

---
+++ Examples/tryGroq-Parallel.ps1
@@ -0,0 +1,31 @@
+Clear-Host
+
+$models = $(
+    'gemma2-9b-it'
+    'llama-3.3-70b-versatile'
+    'llama-3.1-8b-instant'
+    'llama-guard-3-8b'
+    'llama3-70b-8192'
+    'llama3-8b-8192'
+    'mixtral-8x7b-32768'
+    'qwen-qwq-32b'
+    'mistral-saba-24b'
+    'qwen-2.5-coder-32b'
+    'qwen-2.5-32b'
+    'deepseek-r1-distill-qwen-32b'
+    'deepseek-r1-distill-llama-70b'
+    'llama-3.3-70b-specdec'
+    'llama-3.2-1b-preview'
+    'llama-3.2-3b-preview'
+    'llama-3.2-11b-vision-preview'
+    'llama-3.2-90b-vision-preview'
+)
+
+
+$models | ForEach-Object -Parallel {
+    $model = $_
+    Invoke-Completion 'What is the capital of France?' groq:$model | Select-Object Model, Timestamp, Response
+} 
+
+''
+"$($models.Count) models prompted."
