fd53503f02bc08b5205a806645a822784360e0f7
Avri Chen-Roth <the-mentor@users.noreply.github.com>
2025-03-11 13:48:23 +0200

    add SystemRole parameter to xAI Provider

--- Providers/xAI.ps1
+++ Providers/xAI.ps1
@@ -12,6 +12,10 @@
 .PARAMETER Prompt
     The text prompt to send to the model.
 
+.PARAMETER SystemRole
+    This parameter allows you to overwrite the default system role by passing a     
+    hashtable containing the system role information with keys 'role' and 'content'.
+
 .EXAMPLE
     $response = Invoke-XAIProvider -ModelName 'grok-1' -Prompt 'Explain quantum computing'
     
@@ -24,7 +28,16 @@ function Invoke-XAIProvider {
         [Parameter(Mandatory)]
         [string]$ModelName,
         [Parameter(Mandatory)]
-        [string]$Prompt
+        [string]$Prompt,
+        [ValidateScript({
+            if ($_ -is [hashtable] -and $_.ContainsKey('role') -and $_.ContainsKey('content')) {
+                return $true
+            }
+            else {
+                throw "SystemRole must be a hashtable with keys 'role' and 'content'."
+            }
+        })]
+        [hashtable]$SystemRole
     )
     
     $headers = @{
@@ -35,6 +48,9 @@ function Invoke-XAIProvider {
     $body = @{
         'model'    = $ModelName
         'messages' = @(
+            if($SystemRole) {
+                $SystemRole
+            }
             @{
                 'role'    = 'user'
                 'content' = $Prompt
